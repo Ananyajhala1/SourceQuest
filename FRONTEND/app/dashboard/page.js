887ac2cf0e0f6f57a2ad92b337b590d3eb0099a6
Jhanvi <jhanvim77@gmail.com>
2024-02-10 01:15:09 +0530

    dashboard skeleton

--- FRONTEND/app/dashboard/page.js
+++ FRONTEND/app/dashboard/page.js
@@ -2,71 +2,8 @@ import React from 'react';
 
 const Dashboard = () => {
   return (
-    // <div className='flex h-screen bg-white'>
-    //   <div className="w-1/4 bg-orange-500 p-4 flex flex-col items-center">
-    //     <div className="relative mb-4">
-        //   <div className="w-56 h-56 rounded-full bg-gray-300 overflow-hidden">
-        //     <img
-        //       src="https://via.placeholder.com/150"
-        //       alt="Profile"
-        //       className="w-full h-full object-cover"
-        //     />
-        //   </div>
-    //       <div className="absolute inset-0 bg-gray-200 rounded-full clip-custom"></div>
-    //     </div>
-    //     <div className="bg-gray-200 p-6 rounded">
-    //       <h2 className="text-2xl font-bold mb-2">Jhanvi</h2>
-    //       <p className="text-gray-700 text-lg">IIIT Vadodara, IT Branch</p>
-    //       <div className="flex-row mt-4">
-    //         <div className="flex justify-end bg-gray-800 text-white p-2">
-    //           <div className="flex items-center space-x-4">
-    //             <button className="hover:text-gray-300">
-    //               Settings
-    //             </button>
-    //             <button className="hover:text-gray-300">
-    //               Logout
-    //             </button>
-    //           </div>
-    //         </div>
-    //       </div>
-    //     </div>
-    //   </div>
-
-    //   <div className="w-3/4 p-4">
-        // <div className="bg-gray-200 p-10 rounded mb-4">
-        //   <img
-        //     src="https://via.placeholder.com/500x300"
-        //     alt="Placeholder Image"
-        //     className="w-1/4 h-1/3 object-cover rounded"
-        //   />
-        // </div>
-
-        // <div className="flex mb-4">
-        //   <div className="w-1/3 pr-4">
-        //     <div className="bg-gray-200 p-6 rounded">
-        //       <p className="text-gray-700">Contributor at: XYZ</p>
-        //     </div>
-        //   </div>
-        //   <div className="w-1/3 pr-4">
-        //     <div className="bg-gray-200 p-6 rounded">
-        //       <p className="text-gray-700">Stars Ratings: 5</p>
-        //     </div>
-        //   </div>
-        //   <div className="w-1/3">
-        //     <div className="bg-gray-200 p-6 rounded">
-        //       <p className="text-gray-700">Issues Solved: 20</p>
-        //     </div>
-        //   </div>
-        // </div>
-
-        // <div className="bg-gray-200 p-10 rounded">
-        //   <p className="text-gray-700">Roadmap</p>
-        // </div>
-    //   </div>
-    // </div>
-
     <div className='flex flex-col bg-white h-screen'>
-        {/* <header>
+        <header>
         <div className="flex-row">
             <div className="flex justify-end bg-gray-800 text-white p-2">
                <div className="flex items-center space-x-4">
@@ -79,18 +16,18 @@ const Dashboard = () => {
               </div>
             </div>
          </div>
-        </header> */}
+        </header> 
 
-        {/* <div className="bg-gray-200 p-10 rounded mb-4 h-56">
+         <div className="bg-gray-200 p-10 rounded mb-4 h-56 items-center">
           <img
             src="https://via.placeholder.com/500x300"
             alt="Placeholder Image"
             className="w-1/4 h-2/3 object-cover rounded"
           />
-        </div> */}
+        </div> 
 
         <div className='flex flex-row'>
-        {/* <div className="flex-shrink-0 bg-orange-500 w-1/4 h-80 mr-4 ml-2 rounded">
+         <div className="flex-shrink-0 bg-orange-500 w-1/4 h-84 mr-4 ml-2 rounded">
         <div className="w-64 h-64 rounded-full bg-gray-300 overflow-hidden transform translate-y-[-50%] translate-x-[20%]">
             <img
               src="https://via.placeholder.com/150"
@@ -99,17 +36,17 @@ const Dashboard = () => {
             />
           </div>
 
-          </div> */}
-
+          </div> 
+          <div className='flex flex-col'>
           <div className="flex mb-4">
 
-          {/* <div className="w-80 ml-4 mr-4">
+          <div className="w-80 ml-4 mr-4">
             <div className="bg-gray-200 p-6 rounded">
               <p className="text-gray-700">Contributor at: XYZ</p>
             </div>
-          </div> */}
+          </div> 
 
-          {/* <div className="w-80 pr-4">
+          <div className="w-80 pr-4">
             <div className="bg-gray-200 p-6 rounded">
               <p className="text-gray-700">Stars Ratings: 5</p>
             </div>
@@ -119,10 +56,13 @@ const Dashboard = () => {
             <div className="bg-gray-200 p-6 rounded">
               <p className="text-gray-700">Issues Solved: 20</p>
             </div>
-          </div> */}
+          </div> 
 
         </div>
-        
+        <div className="bg-gray-200 ml-4 h-64 p-10 rounded">
+          <p className="text-gray-700">Roadmap</p>
+        </div>
+          </div>
           </div>
           
         </div>
